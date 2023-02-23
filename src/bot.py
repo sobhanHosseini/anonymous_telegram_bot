c4559e0a214278eb3a554dd96b49dec17911c21f
SobhanHosseini <sobhan@Sobhan>
2023-02-23 23:05:48 +0330

    fix some codes

--- src/bot.py
+++ src/bot.py
@@ -1,33 +1,8 @@
 import os
 
 import telebot
-from loguru import logger
 
-from src.dataClass import keyboards as kb
-from src.utils.io import write_json
-
-
-class Bot:
-    """
-    Telegram bot to randomly connect two strangers to talk!
-    """
-    def __init__(self) -> None:
-        self.bot = telebot.TeleBot(os.environ['ANONYMOUS_BOT_TOKEN'])
-        self.echo_all = self.bot.message_handler(func=lambda message: True)(self.echo_all)
-    
-    def run(self):
-        logger.info('Bot is running...')
-        self.bot.infinity_polling()
-    
-    def echo_all(self, message):
-        keyboards = kb.Keyboards() 
-        
-        self.bot.send_message(
-            message.chat.id, message.text,
-            reply_markup=keyboards.main
-            )
-		
-if __name__ == '__main__':
-	logger.info('Bot Started')
-	bot = Bot()
-	bot.run()
+bot = telebot.TeleBot(
+    os.environ['ANONYMOUS_BOT_TOKEN'],
+    parse_mode='HTML'
+    )
---
+++ src/run.py
@@ -0,0 +1,53 @@
+import os
+
+import telebot
+from loguru import logger
+
+from src.dataClass import keyboards as kb
+from src.bot import bot
+from src.filters import IsAdmin
+import emoji
+
+class Bot:
+    """
+    Telegram bot to randomly connect two strangers to talk!
+    """
+    def __init__(self, telebot):
+        self.bot = telebot
+        
+        # add custom filters
+        self.bot.add_custom_filter(IsAdmin())
+        
+        # register handlers
+        self.handlers()
+        
+    def run(self):
+        # run bot 
+        logger.info('Bot is running...')
+        self.bot.infinity_polling()
+        
+    def handlers(self):
+        @self.bot.message_handler(is_admin=True)
+        def admin_of_group(message):
+            self.send_message(message.chat.id, '<strong> You are admin of this chat! </strong>')
+        
+        @self.bot.message_handler(func=lambda _: True)
+        def echo(message):
+            keyboards = kb.Keyboards() 
+            
+            self.send_message(
+                message.chat.id,
+                message.text,
+                reply_markup=keyboards.main
+                )
+    
+    def send_message(self, chat_id, text, reply_markup=None, emojize=True):
+        if emojize:
+            text = emoji.emojize(text, use_aliases=True)
+        
+        self.bot.send_message(chat_id, text, reply_markup=reply_markup)
+		
+if __name__ == '__main__':
+	logger.info('Bot Started')
+	anonymous_bot = Bot(telebot=bot)
+	anonymous_bot.run()
