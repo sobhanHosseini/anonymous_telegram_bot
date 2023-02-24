229aa7890bcf8bdfea359358d156572f80b8fa1b
SobhanHosseini <sobhan@Sobhan>
2023-02-24 17:20:05 +0330

    complete code and conected two stranger

--- src/dataClass/states.py
+++ src/dataClass/states.py
@@ -4,3 +4,4 @@ from dataclasses import dataclass
 class States:
     random_connect:str = 'RANDOM_CONNECT'
     main:str = 'MAIN'
+    connected:str = 'CONNECTED'
--- src/run.py
+++ src/run.py
@@ -39,6 +39,9 @@ class Bot:
         
         @self.bot.message_handler(commands=['start'])    
         def start(message):
+            """
+             /start command handler
+            """
             self.send_message(
                 message.chat.id, 
                 f'Hey <strong>{message.chat.first_name}</strong>'
@@ -54,21 +57,62 @@ class Bot:
             
         @self.bot.message_handler(regexp=emoji.emojize(self.keys.random_connect))
         def random_connect(message):
-            self.send_message(
-                message.chat.id,
-                ':busts_in_silhouette: Connecting you to a random stranger...',
-                reply_markup=self.keyboards.exit
+            """
+            Randomly connect to another user
+            """
+            self.send_message_update_state(
+                chat_id=message.chat.id,
+                text=':busts_in_silhouette: Connecting you to a random stranger...',
+                reply_markup=self.keyboards.exit,
+                state=self.states.random_connect
                 )
-            self.update_state(message.chat.id, self.states.random_connect)
+            
+            other_user = self.find_user(
+                {
+                    'state': states.random_connect,
+                    '_id': {'$ne': message.chat.id}
+                }
+            )
+            
+            if not other_user:
+                return
+            
+            # coneccted two stranger
+            self.conected_two_starnger(first_user_id=message.chat.id, second_user_id=other_user["_id"])
         
         @self.bot.message_handler(regexp=emoji.emojize(self.keys.exit))
         def exit(message):
-            self.send_message(
-                message.chat.id,
-                self.keys.exit,
-                reply_markup=self.keyboards.main
+            """
+            exit from chat or connection state
+            """
+            # update current user
+            self.send_message_update_state(
+                chat_id=message.chat.id,
+                text=self.keys.exit,
+                reply_markup=self.keyboards.main,
+                state=self.states.main
                 )
-            self.update_state(message.chat.id, self.states.main)
+            
+            # get connected to user
+            connected_to = self.find_user(message.chat.id)
+            
+            if not connected_to:
+                return
+            
+            # update connected to user state and terminate the connection  
+            other_chat_id = connected_to['connected_to']
+            
+            self.send_message_update_state(
+                chat_id=other_chat_id,
+                text=self.keys.exit,
+                reply_markup=self.keyboards.main,
+                state=self.states.main
+                )
+            
+            # remove connected users
+            self.update_connected_status(chat_id=message.chat.id, connected_to=None)
+            
+            self.update_connected_status(chat_id=other_chat_id, connected_to=None)
              
         @self.bot.message_handler(is_admin=True)
         def admin_of_group(message):
@@ -79,11 +123,63 @@ class Bot:
         
         @self.bot.message_handler(func=lambda _: True)
         def echo(message):
+            """
+            Echo message to other user
+            """
+            user = self.find_user(message.chat.id)
+            
+            if (not user or user['state'] != self.states.connected or user['connected_to'] is None):
+                return
+            
             self.send_message(
-                message.chat.id,
-                message.text,
-                reply_markup=self.keyboards.main
+                user['connected_to'],
+                message.text
+            )
+    
+    def conected_two_starnger(first_user_id, second_user_id):
+        """
+        connected two stranger.
+        """
+        # update fiest user state
+        self.send_message_update_state(
+            chat_id=first_user_id,
+            text=f'Connected to {second_user_id} ....',
+            state=self.states.connected
+            )
+        
+        # update second user state
+        self.send_message_update_state(
+            chat_id=second_user_id,
+            text=f'Connected to {first_user_id} ....',
+            state=self.states.connected
+            )
+        
+        # store first connected users
+        self.update_connected_status(chat_id=first_user_id, connected_to=second_user_id)
+        
+        # store second connected user
+        self.update_connected_status(chat_id=second_user_id, connected_to=first_user_id)
+        
+    
+    def update_connected_status(self, chat_id, connected_to=None):
+        """
+        update conected_to status
+        """
+        db.users.update_one(
+                {'_id': chat_id},
+                {'$set': {'connected_to': connected_to}}
+            )
+    
+    def send_message_update_state(self, chat_id, text, reply_markup, state):
+        """
+        send message and change state of user
+        """
+        self.send_message(
+                chat_id,
+                text,
+                reply_markup=reply_markup
                 )
+        self.update_state(chat_id, state)
         
     def send_message(self, chat_id, text, reply_markup=None, emojize=True):
         if emojize:
@@ -99,6 +195,14 @@ class Bot:
             {'_id': chat_id}, 
             {'$set':{'state':state}}
             )
+    
+    def find_user(self, id=None, condition=None):
+        default = {'_id': id}
+        
+        if condition != None:
+            default.update(condition)
+            
+        return db.users.find_one(default)
         
         
 if __name__ == '__main__':
