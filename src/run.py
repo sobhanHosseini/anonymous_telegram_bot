6e0ea5dc26c450a9c753c91dd6fb4d42bdb76c6d
SobhanHosseini <sobhan@Sobhan>
2023-02-24 17:26:43 +0330

    fix exit function

--- src/run.py
+++ src/run.py
@@ -95,8 +95,8 @@ class Bot:
             
             # get connected to user
             connected_to = self.find_user(message.chat.id)
-            
-            if not connected_to:
+          
+            if not connected_to['connected_to']:
                 return
             
             # update connected to user state and terminate the connection  
